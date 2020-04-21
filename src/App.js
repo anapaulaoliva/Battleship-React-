3de5a35845ff013ae3eb8e24f6ebe970111bbf96
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-21 17:29:15 -0500

    Modularization of files and folders

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import logo from './BattleshipMainLogo.png';
-import logoBackground from './BattleshipBackgroundLogo.png';
-import gitHubLogo from './github-logo.JPG';
-import './App.css';
+import Welcome from './views/welcome.js';
+
 
 function App() {
+  //TODO: Aqui solo debe ir Router y se deben importar las vistas.
   return (
-    <main className="App">
-      <div className="App-main">
-        <img src={logo} className="App-logo-top" alt="main-logo" />
-        <img src={logoBackground} className="App-logo-bottom" alt="logo-background" />
-        <button type="button" className="App-main-btn">Start The Battle!</button>
-      </div>
-    <footer className="App-footer">
-        <img src={gitHubLogo} className="App-footer-logo" alt="github-logo" />''
-        <a
-          className="App-link"
-          href="https://github.com/anapaulaoliva/Battleship-React"
-          target="_blank"
-          rel="noopener noreferrer"
-          alt="source-code"
-        > 
-        source code
-        </a>
-        <br /><br />
-      </footer>
-    </main>
+    <Welcome/>
   );
 }
 
