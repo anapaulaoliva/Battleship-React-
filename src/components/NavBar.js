53ef9bea0b431be893e1cc305bb3cc9c4590f97a
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-22 10:59:48 -0500

    Implements AppBar component from Material-UI React

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Typography, Toolbar } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+class NavBar extends Component {
+    render() {
+        const { classes } = this.props;
+
+        return(
+        <>
+        <AppBar className={classes.NavColor} position="static">
+            <Toolbar variant="dense">
+                <Typography variant="h6" component="p">
+                    <Link to="/">
+                        Battleship
+                    </Link>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </>
+        )
+    }
+}
+
+export default withStyles({
+    NavColor: {
+        backgroundColor: 'black',
+        position: 'fixed',
+        fontFamily: 'monospace', 
+        color: 'green',
+        display: 'flex',
+        }
+    }) (NavBar);
+
