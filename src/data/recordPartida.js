e177fe2f5219d2a96ab3eee76ade66b30a3de7ee
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-28 10:07:05 -0500

    Modelling data for firebase collections

---
+++ src/data/recordPartida.js
@@ -0,0 +1,14 @@
+partida = {
+    ASD34:{
+        P1: {
+            name:"Anapau",
+            scord: 0
+        }
+        p2:{
+            name: "frank",
+            score: 2
+        }
+    },
+    4353S:{},
+    DF45SD:{}
+}
