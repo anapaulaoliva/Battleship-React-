4c775fe0fa861ee3150106579155687e0abebf26
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-29 22:38:37 -0500

    First iteration trough .add and .set methods Firestore

--- src/components/Fieldset.js
+++ src/components/Fieldset.js
@@ -1,68 +1,71 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import firebase, { firestore } from 'firebase';
+import { useHistory } from 'react-router-dom';
+import  useForm  from './useForm.js';
+import validate from './validateForm.js';
 import '../styles/Fieldset.css';
 
 const Fieldset = () => {
-    const [docNamePlayersState, setDocNamePlayersState] = useState({
-        player1: {
-            name: '',
-            score: 0
-        },
-        player2: {
-            name: '',
-            score: 0
-        }
-        
-    });
+    // useState recibe un parámetro: el valor inicial del estado y devuelve un array de dos posiciones:
+    //  la primera tiene el valor del estado
+    //  la segunda el método para actualizar el estado
+    const { handleChange, handleSubmit, values, errors } = useForm(submit, validate);
+    const history = useHistory();
 
-    const handleInputChange = (e) => {
-        
-        setDocNamePlayersState({...docNamePlayersState,  [e.target.id] : {name: e.target.value, score: 0}});
-        console.log(docNamePlayersState)
-        
+    function submit() {
+        firestore().collection("recordJugadores").add(values)
+        .then(() => {
+            history.push('/home');
+            console.log('Record successfully created!', values);
+        })
     }
-
-    const addPlayers = ( ) => {
-        console.log(docNamePlayersState);
-        
-        //TODO:validar que los inputs no esten vacios
-        firestore().collection('recordPartidas').add(docNamePlayersState)
-    }
+            
+            
+            /*orderBy("timestamp", "desc").onSnapshot(snapshot => {      
+            //code goes here*/
 
     return(
         <fieldset className="form">
             <legend>Enter your names:</legend>
             <div>
-                <label htmlFor="player1">
-                    <input 
-                        type="text" 
-                        id="player1" 
-                        name="player1"
-                        value={docNamePlayersState.player1.name} 
-                        onChange={handleInputChange}
-                        placeholder="Player1" />
-                </label>
-                <label htmlFor="player2">
-                    <input 
-                        type="text" 
-                        id="player2" 
-                        name="player2"
-                        value={docNamePlayersState.player2.name} 
-                        onChange={handleInputChange}
-                        placeholder="Player2" />
-                </label>
+                <label htmlFor="player1"></label>
+                    <div>
+                        <input 
+                            className={`${errors.player2 && "inputError"}`}
+                            type="text" 
+                            id="player1" 
+                            name="player1"
+                            value={/*values.player1.name*/values.player1} 
+                            onChange={handleChange}
+                            placeholder="Player1" />
+                            {errors.player1 &&
+                            <p className="error">{errors.player1}</p>}
+                    </div>
+                    
+                <label htmlFor="player2"></label>
+                    <div>
+                        <input 
+                            className={`${errors.player2 && "inputError"}`}
+                            type="text" 
+                            id="player2" 
+                            name="player2"
+                            value={/*values.player2.name*/ values.player2} 
+                            onChange={handleChange}
+                            placeholder="Player2" />
+                            {errors.player2 &&
+                            <p className="error">{errors.player2}</p>}
+                    </div>
             </div>
-                <Link to='/home'>
+               
                     <button 
                         type="button" 
                         id = "saveButton"
                         className="MainBtn"
-                        onClick={addPlayers}
+                        onClick={handleSubmit}
                     >
                     Start The Battle!
                     </button>
-                </Link>
+               
         </fieldset>
     );
 }
