c29b7514353753a26037998fe62dcd0b99e2997d
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-29 22:53:20 -0500

    Added firebase dep

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Firebase from './Firebase.js';
+import Firebase from './Firebase';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
