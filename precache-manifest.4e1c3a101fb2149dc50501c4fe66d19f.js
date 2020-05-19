6d6ed24e1b8aca0901f382a6c8dfb846bc8ca746
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-05-18 22:53:56 -0500

    Updates

---
+++ precache-manifest.4e1c3a101fb2149dc50501c4fe66d19f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7457af36a338c8a0a251a61a01825c9a",
+    "url": "/battleship-react/index.html"
+  },
+  {
+    "revision": "2093eea1ac5a167c9f75",
+    "url": "/battleship-react/static/css/main.13a7c667.chunk.css"
+  },
+  {
+    "revision": "37e087266acb8c821cb7",
+    "url": "/battleship-react/static/js/2.35bbc098.chunk.js"
+  },
+  {
+    "revision": "cfcdebbd5c361eb80dad4bc286f1bc4f",
+    "url": "/battleship-react/static/js/2.35bbc098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2093eea1ac5a167c9f75",
+    "url": "/battleship-react/static/js/main.ae9f5376.chunk.js"
+  },
+  {
+    "revision": "bf8f1a8a4dad77e125c5",
+    "url": "/battleship-react/static/js/runtime-main.bc7e1246.js"
+  },
+  {
+    "revision": "4dc05d30d97fc7e98b3e24daed788c7b",
+    "url": "/battleship-react/static/media/BattleshipBackgroundLogo.4dc05d30.png"
+  },
+  {
+    "revision": "d8528b5e53d1c774ec6a0fa882492163",
+    "url": "/battleship-react/static/media/BattleshipMainLogo.d8528b5e.png"
+  },
+  {
+    "revision": "df87c968de22fd8f2f5355737367e36e",
+    "url": "/battleship-react/static/media/board-border.df87c968.png"
+  },
+  {
+    "revision": "283001a1c02066f1f6b1a7224068fe51",
+    "url": "/battleship-react/static/media/github-logo.283001a1.JPG"
+  }
+]);
